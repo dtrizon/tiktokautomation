cfdb9829179595389f5315cf9490c697274a2a5f
Damrey T. Rizon <damreyrizont@gmail.com>
2023-01-22 15:17:32 +0800

    Update nickclements.py

--- nickclements.py
+++ nickclements.py
@@ -3,6 +3,9 @@
 #export CPPFLAGS="${CPPFLAGS} -I$(brew --prefix openssl)/include"
 #export LDFLAGS="${LDFLAGS} -L$(brew --prefix openssl)/lib"
 #https://drive.google.com/drive/u/0/folders/15nU34WeQUOgGaB-pGQqlee3UVlvz-sIX
+#export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+#export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+#export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 
 import sys
 from PyQt5.QtCore import Qt, QMimeData, QUrl
